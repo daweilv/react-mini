df677cebd3a659666c9c5a6d24638243ab265d61
daweilv <lvdawei1970@gmail.com>
2018-08-21 11:18:50 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,174 +1,39 @@
 import React, { Component } from "./react";
 import { render } from "./react-dom";
 
-class Root extends Component {
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="HelloWorld">
+        <Hello style={{ background: "grey" }} />
+        <h3 className="World" style={{ color: "red" }}>
+          The Wonderful World!
+        </h3>
+      </div>
+    );
+  }
+}
+
+class Hello extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      flag: true
-    };
+    this.state = { count: 0 };
   }
 
   render() {
-    const { flag } = this.state;
-    if (!flag) return null;
+    const { style } = this.props;
     return (
-      <div
-        style={{ backgroundColor: "grey", padding: "30px" }}
+      <h1
+        className="Hello"
+        style={style}
         onClick={() => {
-          this.setState({
-            flag: false
-          });
+          this.setState({ count: ++this.state.count });
         }}
       >
-        <Parent />
-      </div>
+        Hello!! Click me {this.state.count} times
+      </h1>
     );
   }
 }
 
-class Parent extends Component {
-  constructor(props) {
-    super(props);
-    console.log("parent:constructor invoked");
-    this.state = {
-      child1Style: { color: "green" },
-      child2Style: { color: "red" }
-    };
-  }
-
-  componentWillMount() {
-    console.log("parent:componentWillMount invoked");
-  }
-
-  componentDidMount() {
-    console.log("parent:componentDidMount invoked");
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log("parent:componentWillReceiveProps invoked");
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("parent:shouldComponentUpdate invoked");
-    return true;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log("parent:componentWillUpdate invoked");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("parent:componentDidUpdate invoked");
-  }
-
-  componentWillUnmount() {
-    console.log("parent:componentWillUnmount invoked");
-  }
-
-  render() {
-    console.log("parent:render invoked");
-    return (
-      <div
-        className="parent"
-        style={{ backgroundColor: "#3c3c3c" }}
-        onClick={e => {
-          e.stopPropagation();
-          this.setState({
-            child1Style: { color: "yellow" },
-            child2Style: { color: "blue" }
-          });
-        }}
-      >
-        <Child1 style={this.state.child1Style} />
-        <Child2 style={this.state.child2Style} />
-      </div>
-    );
-  }
-}
-
-class Child1 extends Component {
-  constructor(props) {
-    super(props);
-    console.log("child1:constructor invoked");
-  }
-
-  componentWillMount() {
-    console.log("child1:componentWillMount invoked");
-  }
-
-  componentDidMount() {
-    console.log("child1:componentDidMount invoked");
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log("child1:componentWillReceiveProps invoked");
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("child1:shouldComponentUpdate invoked");
-    return true;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log("child1:componentWillUpdate invoked");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("child1:componentDidUpdate invoked");
-  }
-
-  componentWillUnmount() {
-    console.log("child1:componentWillUnmount invoked");
-  }
-
-  render() {
-    console.log("child1:render invoked");
-    const { style } = this.props;
-    return <div style={style}>child1</div>;
-  }
-}
-
-class Child2 extends Component {
-  constructor(props) {
-    super(props);
-    console.log("child2:constructor invoked");
-  }
-
-  componentWillMount() {
-    console.log("child2:componentWillMount invoked");
-  }
-
-  componentDidMount() {
-    console.log("child2:componentDidMount invoked");
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log("child2:componentWillReceiveProps invoked");
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("child2:shouldComponentUpdate invoked");
-    return true;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log("child2:componentWillUpdate invoked");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("child2:componentDidUpdate invoked");
-  }
-
-  componentWillUnmount() {
-    console.log("child2:componentWillUnmount invoked");
-  }
-
-  render() {
-    console.log("child2:render invoked");
-    const { style } = this.props;
-    return <div style={style}>child2</div>;
-  }
-}
-
-render(<Root />, document.getElementById("app"));
+render(<HelloWorld />, document.getElementById("app"));
--- src/react-dom/index.js
+++ src/react-dom/index.js
@@ -9,13 +9,13 @@ const ReactDOM = (function() {
   let cdupQueque = null;
   let stage = null;
   // 挂载入口
-  function render(rootVNode, domContainer) {
+  function render(rootVnode, domContainer) {
     stage = Stage.Mouting;
     cdmQueque = new Queue();
-    if (!rootVNode) return;
+    if (!rootVnode) return;
     empty(domContainer);
     // 将 DOM Element 添加到 container
-    domContainer.append(complieVNode2DOM(rootVNode));
+    domContainer.append(parseVnode(rootVnode));
     // 按顺序 invoke cdm 事件
     invokeComponentDidMountQueue();
   }
@@ -24,95 +24,170 @@ const ReactDOM = (function() {
   function rerender(inst) {
     stage = Stage.Updating;
     cdupQueque = new Queue();
-    let prevEle = inst._ele;
-    let ele = complieVNode2DOM(inst.render());
+    let oldNode = inst._ele;
+    // todo: lifecycle 需要更新，parseVnode 考虑与 diffVnode 分离
+    let ele = parseVnode(inst.render(), oldNode);
     inst._ele = ele;
     ele._inst = inst;
     addComponentDidUpdateQueue(inst);
-    prevEle.parentElement.replaceChild(ele, prevEle);
+    // oldNode.parentNode.replaceChild(ele, oldNode);
     // 按顺序 invoke cdup 事件
     invokeComponentDidUpdateQueue();
   }
 
   // 将类 DOM 对象渲染成 DOM Element
-  // 依次处理 number  / string / function & class 组件 / 普通 HTML 标签
-  function complieVNode2DOM(vnode) {
-    if (!vnode) return null;
-    if (typeof vnode === "string" || typeof vnode === "number") {
-      const ele = document.createTextNode(vnode);
-      return ele;
+  // 依次处理 number / string / function & class 组件 / 普通 HTML 标签
+  function parseVnode(vnode, oldNode) {
+    if (typeof vnode === "number") vnode = String(vnode);
+    if (typeof vnode === "string") {
+      let node = parseTextVnode(vnode, oldNode);
+      return node;
     }
 
     if (typeof vnode.type === "function") {
-      const comp = vnode.type;
-      const props = vnode.props;
-      let ele;
-      let inst;
-      // 继承了 React.Component 的组件拥有 render 方法
-      if (comp.prototype && comp.prototype.render) {
-        inst = new comp(props);
-        if (stage === Stage.Mouting) {
-          // cwm 事件在实例创建之后，render 之前 invoke
-          invokeComponentWillMount(inst);
-        } else if (stage === Stage.Updating) {
-          // cwr 事件在 scu 之前 invoke
-          invokeComponentWillReceiveProps(inst);
-          if (
-            !inst.shouldComponentUpdate ||
-            (inst.shouldComponentUpdate &&
-              inst.shouldComponentUpdate(vnode.props, inst.state))
-          ) {
-            invokeComponentWillUpdate(inst);
-          }
-        }
-
-        ele = complieVNode2DOM(inst.render());
-        inst._ele = ele;
-        ele._inst = inst;
-      } else {
-        ele = complieVNode2DOM(comp(props));
-      }
-
-      if (props.children) {
-        if (Array.isArray(props.children)) {
-          props.children.forEach(childVnode =>
-            ele.append(complieVNode2DOM(childVnode))
-          );
-        } else {
-          ele.append(complieVNode2DOM(props.children));
-        }
-      }
-      if (stage === Stage.Mouting) {
-        // 添加到 cdm 事件队列中，等待 invoke
-        addComponentDidMountQueue(inst);
-      } else if (stage === Stage.Updating) {
-        // 添加到 cdup 事件队列中，等待 invoke
-        addComponentDidUpdateQueue(inst);
-      }
-
-      return ele;
+      let node = parseFuncVnode(vnode, oldNode);
+      return node;
     }
 
-    // 普通 HTML 标签
-    // todo: svg 需要 NS 支持
-    const ele = document.createElement(vnode.type);
+    let node = parseEleVnode(vnode, oldNode);
+    return node;
+  }
+
+  function parseTextVnode(vnode, oldNode) {
+    const TEXT_NODE = 3;
+    if (oldNode) {
+      if (oldNode.nodeType === TEXT_NODE) {
+        if (oldNode.textContent !== vnode) {
+          oldNode.textContent = vnode;
+        }
+        return oldNode;
+      } else {
+        const node = document.createTextNode(vnode);
+        replaceNode(node, oldNode);
+        return node;
+      }
+    }
+    const node = document.createTextNode(vnode);
+    return node;
+  }
+
+  function parseFuncVnode(vnode, oldNode) {
+    const comp = vnode.type;
     const props = vnode.props;
-    // 添加/更新 attributes
-    updateAttributes(ele, props);
+    let ele;
+    let inst;
+    // 继承了 React.Component 的组件拥有 render 方法
+    if (comp.prototype && comp.prototype.render) {
+      if (stage === Stage.Mouting) {
+        inst = new comp(props);
+        // cwm 事件在实例创建之后，render 之前 invoke
+        invokeComponentWillMount(inst);
+      } else if (stage === Stage.Updating) {
+        // cwr 事件在 scu 之前 invoke
+        inst = oldNode._inst;
+        invokeComponentWillReceiveProps(inst);
+        inst.props = vnode.props;
+        if (
+          !inst.shouldComponentUpdate ||
+          (inst.shouldComponentUpdate &&
+            inst.shouldComponentUpdate(vnode.props, inst.state))
+        ) {
+          invokeComponentWillUpdate(inst);
+        }
+      }
+
+      ele = parseVnode(inst.render(), inst._ele);
+      inst._ele = ele;
+      ele._inst = inst;
+    } else {
+      ele = parseVnode(comp(props));
+    }
 
     if (props.children) {
-      if (Array.isArray(props.children)) {
-        props.children.forEach(childVnode =>
-          ele.append(complieVNode2DOM(childVnode))
-        );
-      } else {
-        ele.append(complieVNode2DOM(props.children));
-      }
+      parseVnodeChildren(props.children, ele);
+    }
+    if (stage === Stage.Mouting) {
+      // 添加到 cdm 事件队列中，等待 invoke
+      addComponentDidMountQueue(inst);
+    } else if (stage === Stage.Updating) {
+      // 添加到 cdup 事件队列中，等待 invoke
+      addComponentDidUpdateQueue(inst);
     }
     return ele;
   }
 
+  function parseEleVnode(vnode, oldNode) {
+    let ele;
+    const props = vnode.props;
+    if (oldNode && isSameNode(vnode, oldNode)) {
+      ele = oldNode;
+    } else {
+      // 普通 HTML 标签
+      // todo: svg 需要 NS 支持
+      ele = document.createElement(vnode.type);
+    }
+
+    // 添加/更新 attributes
+    updateAttributes(ele, props);
+
+    if (props.children) {
+      parseVnodeChildren(props.children, ele);
+    }
+    return ele;
+  }
+
+  function parseVnodeChildren(vnodeChildren, oldParentNode) {
+    if (!vnodeChildren) return;
+    if (Array.isArray(vnodeChildren)) {
+      diffChildren(vnodeChildren, oldParentNode);
+    } else {
+      diffChildren([vnodeChildren], oldParentNode);
+    }
+  }
+
+  function diffChildren(vnodes, parentEle) {
+    if (!parentEle) {
+      vnodes.forEach(childVnode => parentEle.append(parseVnode(childVnode)));
+      return;
+    }
+    let eles = Array.prototype.map.call(parentEle.childNodes, i => i);
+    for (let vnodeIndex = 0; vnodeIndex < vnodes.length; vnodeIndex++) {
+      let vnode = vnodes[vnodeIndex];
+      let ele;
+      for (let eleIndex = 0; eleIndex < eles.length; eleIndex++) {
+        let e = eles[eleIndex];
+        if (e && isSameNode(vnode, e)) {
+          ele = e;
+          eles[eleIndex] = undefined;
+          break;
+        }
+      }
+      let newEle = parseVnode(vnode, ele);
+      let oldEle = parentEle.childNodes[vnodeIndex];
+      if (newEle != oldEle) {
+        if (!oldEle) {
+          console.log("append", newEle);
+
+          parentEle.appendChild(newEle);
+        } else {
+          console.log("insert", newEle);
+
+          parentEle.insertBefore(newEle, oldEle);
+        }
+      }
+    }
+
+    for (let eleIdx = 0; eleIdx < eles.length; eleIdx++) {
+      if (eles[eleIdx] !== undefined) {
+        // todo: cwun
+        console.log("remove ", eles[eleIdx]);
+        parentEle.removeChild(eles[eleIdx]);
+      }
+    }
+  }
+
   function updateAttributes(ele, attrs) {
+    //todo diff attr
     for (const name in attrs) {
       if (name === "children") return;
       updateAttribute(ele, name, attrs[name]);
@@ -123,7 +198,9 @@ const ReactDOM = (function() {
   function updateAttribute(ele, name, value) {
     if (name.indexOf("on") === 0) {
       name = name.toLowerCase();
-      ele[name] = value;
+      if (ele[name] !== value) {
+        ele[name] = value;
+      }
     } else if (name === "style") {
       Object.assign(ele.style, value);
     } else {
@@ -144,6 +221,18 @@ const ReactDOM = (function() {
     while (ele.firstChild) {
       ele.removeChild(ele.firstChild);
     }
+  }
+
+  function isSameNode(vnode, ele) {
+    if (typeof vnode === "string" || typeof vnode === "number") {
+      return ele && ele.nodeType === 3;
+    }
+
+    if (typeof vnode.type === "function") {
+      return ele && ele._inst && ele._inst.constructor === vnode.type;
+    }
+
+    return ele && vnode.type === ele.tagName.toLowerCase();
   }
 
   function invokeComponentWillMount(inst) {
@@ -178,6 +267,10 @@ const ReactDOM = (function() {
       let inst = cdupQueque.dequeue();
       inst.componentDidUpdate && inst.componentDidUpdate();
     }
+  }
+
+  function replaceNode(node, oldNode) {
+    oldNode.parentNode && oldNode.parentNode.replaceChild(node, oldNode);
   }
 
   return {
